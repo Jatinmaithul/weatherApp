c31e267d91dd33a7e5eb361a777b5ca173cbcec7
Jatin Maithul <54944001+Jatinmaithul@users.noreply.github.com>
2021-04-03 14:03:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+const api = {
+    key:"22c34eb319000b9e7895508b7091888c",
+    baseurl:"https://api.openweathermap.org/data/2.5/"
+}
+
+
+const searchBox = document.querySelector(".search-box");
+searchBox.addEventListener("keypress", setQuery);
+
+function setQuery(e){
+    if(e.keyCode == 13){
+        getResults(searchBox.value);
+    }
+}
+
+
+function getResults(query){
+    fetch(`${api.baseurl}weather?q=${query}&units=metric&APPID=${api.key}`)
+    .then(weather =>{
+        return weather.json();})
+        .then(displayResults);
+}
+
+
+function displayResults(weather){
+    console.log(weather);
+    let city = document.querySelector(".location .city");
+    city.innerText = `${weather.name}, ${weather.sys.country}`;
+
+    let now = new Date();
+    let date = document.querySelector(".location .date");
+    date.innerText = dateBuilder(now);
+    
+
+    let temp = document.querySelector(".current .temp");
+    temp.innerHTML = `${Math.round(weather.main.temp)}<span>&#176;c</span>`;
+
+    let weatherEl = document.querySelector(".current .weather");
+    weatherEl.innerText = weather.weather[0].main;
+
+    let hilow = document.querySelector(".hi-low");
+    hilow.innerText = `${Math.round(weather.main.temp_min)}°c / ${Math.round(weather.main.temp_max)}°c`;}
+
+function dateBuilder(d){
+    let months =["January","February","March","April","May","June","July","August","September","October","November","December"];
+    let days=["Monday","Tuesday","Wednesday","Thrusday","Friday","Saturday","Sunday"];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`;
+}
